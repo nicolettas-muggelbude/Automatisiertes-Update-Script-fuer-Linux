1158c40a1e9960857380a9fd5ae1d6a7a307ed86
nicolettas-muggelbude <nicolettas@muggelbude.eu>
2025-11-05 23:50:11 +0100

    Datei entfernt

--- git_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git init
-git config user.name "nicolettas-muggelbude"
-git config user.email "nicolettas@muggelbude.eu"
-git add .
-git commit -m "Erster Commit"
-git remote add origin git@github.com:nicolettas-muggelbude/Automatisiertes-Update-Script-fuer-Linux.git
-git push -u origin main
