0fa02b6c94325ffdd07aab88ab72aec348930931
nicolettas-muggelbude <nicolettas@muggelbude.eu>
2025-11-09 17:59:26 +0100

    Entferne log_view_neu.sh und log_view_new50.sh

--- log_view_neu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-### Neueste Logdatei anzeigen:
-
-ls -lt /var/log/system-updates/ | head -n 2
-cat /var/log/system-updates/update_*.log
-
-read -p "Drücke Return-Taste um zu schließen ..."
